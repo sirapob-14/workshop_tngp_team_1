d9c66f29173f409bb5434eac4db86437820985d8
“ariyapon-unakul” <ariyapon.un@tnds.com>
2024-08-16 15:39:00 +0700

    feat: mod

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
- 
+
 func main() {
 	fmt.Println("Hello world!")
+	mod(5, 2)
+}
+
+func mod(a, b int) {
+	result := a % b
+	output := fmt.Sprintf("a %% b = %d %% %d = %d", a, b, result)
+	fmt.Println(output)
 }
