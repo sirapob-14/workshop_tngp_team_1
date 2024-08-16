d8903deaf35ffe36203b71c574efeca63c6e60fb
numphoom.le <numphoom.le@tnds.com>
2024-08-16 15:41:18 +0700

    feat: add divide function

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import "fmt"
+
+func divide(a, b int){
+	if b == 0 {
+		fmt.Println("Cannot divided by 0")
+	}
+
+	fmt.Printf("a/b = %d/%d = %d \n",a ,b, a/b)
+ }
  
 func main() {
 	fmt.Println("Hello world!")
+	divide(100,10)
 }
