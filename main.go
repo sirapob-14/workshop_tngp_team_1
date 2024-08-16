b233a16eebd1b02d4d3555f6520588367d543b7b
Thinapat Pakdee <thinnapak.ph@tnds.com>
2024-08-16 15:47:11 +0700

    feat: add multiply function

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import "fmt"
- 
+
 func main() {
 	fmt.Println("Hello world!")
+	a := 40
+	b := 50
+	multiply(a,b)
+}
+
+func multiply(a, b int) int {
+	result := a * b
+	fmt.Println("a * b =",a,"*",b,"=",result)
+	return result
 }
